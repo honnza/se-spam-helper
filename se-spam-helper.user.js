d646605d3b44af2d39811ac32d7ef79dc6a97ad8
Jan Dvorak <honnza+github@gmail.com>
2013-08-18 16:46:51 +0200

    Hider incorrectly relied on `seen_twice`.
    renamed `seen_twice` to `hidden_today`. 
    `hidden_today` no longer persisted.

--- se-spam-helper.user.js
+++ se-spam-helper.user.js
@@ -3,10 +3,12 @@
 // @description   filter for the stack exchange real time question viewer,
 // @description   aiding in identification and removal of network-wide obvious spam
 // @match         http://stackexchange.com/questions?tab=realtime
-// @version       1.2.1
+// @version       1.3
 // ==/UserScript==
 
 (function(){
+  localStorage.removeItem("spam-helper-seen_twice");
+  
   var ws = new WebSocket("ws://sockets.ny.stackexchange.com");
   ws.onmessage = onmessage;
   ws.onopen = function(){ws.send("155-questions-active")};
@@ -24,9 +26,9 @@
   var seen = {};
   var seen_ary = JSON.parse(localStorage.getItem("spam-helper-seen"))||[];
   seen_ary.forEach(function(x){seen[x] = true});
-  var seen_twice = {};
-  var seen_twice_ary = JSON.parse(localStorage.getItem("spam-helper-seen_twice"))||[];
-  seen_twice_ary.forEach(function(x){seen_twice[x] = true});
+  seen_ary = JSON.parse(localStorage.getItem("spam-helper-seen_twice"))||[]; //upgrade from v1.2
+  seen_ary.forEach(function(x){seen[x] = true});
+  var hidden_today = {};
   var seen_today = {};
   window.addEventListener("unload", onbeforeunload);
 
@@ -62,11 +64,11 @@
 
     seen_today[classname] = true;
 
-    if(!seen_twice[classname]){
+    if(!hidden_today[classname]){
       if(seen[classname]){
         css.textContent += "." + classname +
           " {height: 1em; overflow: hidden; padding-top: 0; padding-bottom: 0}\n";
-        seen_twice[classname] = true;
+        hidden_today[classname] = true;
       }else{
         //if(/[^a-z0-9\s]{6}/i.test(title)){
         //  css.textContent += "." + classname + " {background-color: #FCC}\n";
@@ -84,17 +86,15 @@
 
   function onbeforeunload(){
     console.log('unloading');
+    localStorage.removeItem("spam-helper-seen_twice"); //upgrade from v1.2
     try{
-      var seen_ary = Object.keys(seen).filter(function(x){return !seen_twice[x]});
+      var seen_ary = Object.keys(seen);
       var seen_twice_ary = Object.keys(seen_twice);
       localStorage.setItem("spam-helper-seen", JSON.stringify(seen_ary));
-      localStorage.setItem("spam-helper-seen_twice", JSON.stringify(seen_twice_ary));
     }catch(ex){
       console.log('unloading failed');
       seen_ary = seen_ary.filter(function(x){return seen_today[x]});
-      seen_twice_ary = seen_twice_ary.filter(function(x){return seen_today[x]});
       localStorage.setItem("spam-helper-seen", JSON.stringify(seen_ary));
-      localStorage.setItem("spam-helper-seen_twice", JSON.stringify(seen_twice_ary));
       console.log('pruned data');
     }
   };
