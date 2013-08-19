812de67440b724c6d7e75aa9ecd54198081db014
Jan Dvorak <honnza+github@gmail.com>
2013-08-19 05:12:33 +0200

    1.5 `@match` -> `@include`: firefox support

--- se-spam-helper.user.js
+++ se-spam-helper.user.js
@@ -2,8 +2,8 @@
 // @name          Stack Exchange spam helper
 // @description   filter for the stack exchange real time question viewer,
 // @description   aiding in identification and removal of network-wide obvious spam
-// @match         http://stackexchange.com/questions?tab=realtime
-// @version       1.4.10
+// @include       http://stackexchange.com/questions?tab=realtime
+// @version       1.5
 // ==/UserScript==
 
 (function(){
