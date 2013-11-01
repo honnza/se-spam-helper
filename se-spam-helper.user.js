e8e93b39b430c1dfed0032d33592986c54c82066
Jan Dvorak <honnza@gmail.com>
2013-11-01 18:41:20 +0100

    1.4.5 - added yet another fashion related keyword

--- se-spam-helper.user.js
+++ se-spam-helper.user.js
@@ -3,7 +3,7 @@
 // @description   filter for the stack exchange real time question viewer,
 // @description   aiding in identification and removal of network-wide obvious spam
 // @include       http://stackexchange.com/questions?tab=realtime
-// @version       1.7.4
+// @version       1.7.5
 // ==/UserScript==
 
 (function(){
@@ -105,7 +105,7 @@
           notify("Potentially offensive message detected");
         }else if(/(?:[^a-z ] *){9,}/i.test(title)
           || is.mostlyUppercase(title)
-          || /\b(vs?|live|watch|free|online|download|nike|training|dress|buy|here is|porn)\b/i.test(title)
+          || /\b(vs?|live|watch|free|online|download|nike|training|dress|fashion|buy|here is|porn)\b/i.test(title)
         ){
           css.textContent += "." + classname + " {background-color: #FCC}\n";
           notify("Highly suspicious message detected");
