6620027e58f9dcda748622548d000403c2ca6fac
Jan Dvorak <honnza@gmail.com>
2014-08-17 11:59:39 +0200

    3.0.2 - the per-site URLs are _not_ relative to the realtime page...

--- se-spam-helper.user.js
+++ se-spam-helper.user.js
@@ -3,7 +3,7 @@
 // @description   filter for the stack exchange real time question viewer,
 // @description   aiding in identification and removal of network-wide obvious spam
 // @include       http://stackexchange.com/questions?tab=realtime
-// @version       3.0.1.1
+// @version       3.0.2
 // ==/UserScript==
 
 /* global unsafeWindow, GM_xmlhttpRequest */
@@ -111,12 +111,11 @@
     hiderInstall();
   }
   
-  function scrapePerSiteQuestion(html, siteId){
+  function scrapePerSiteQuestion(html, site){
     var question = new DOMParser().parseFromString(html, "text/html")
       .getElementsByClassName("question-summary")[0];
-    var qLink = document.createElement("a");
-    //set the owner document for the purposes of URL resolution
-    qLink.href = question.querySelector("a.question-hyperlink").getAttribute("href");
+    var qLink = "//" + siteNameToHostName(site) 
+              + question.querySelector("a.question-hyperlink").getAttribute("href");
     onQuestionActive({
       body: $(".excerpt", question).html().trim(),
       link: qLink.href,
